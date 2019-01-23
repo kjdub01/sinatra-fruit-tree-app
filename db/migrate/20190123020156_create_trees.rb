e4f80954ee2565c934acea223e148739cf475c40
kate jewett-williams <kjewettwilliams@gmail.com>
2019-01-22 19:15:45 -0700

    update tree migration to include homeowner_id and set fruit_weight to an integer

--- db/migrate/20190123020156_create_trees.rb
+++ db/migrate/20190123020156_create_trees.rb
@@ -3,7 +3,8 @@ class CreateTrees < ActiveRecord::Migration
     create_table :trees do |t|
     	t.string :variety
      	t.string :size
-     	t.string :fruit_weight
+     	t.integer :fruit_weight
+     	t.integer :homeowner_id
      	t.timestamps null: false
     end
   end
