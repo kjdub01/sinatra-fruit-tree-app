9c2ded473c97439759277f7988d2b489808c526d
kate jewett-williams <kjewettwilliams@gmail.com>
2019-01-22 19:30:36 -0700

    update homeowner table to have a first and last name

--- db/migrate/20190123015147_create_homeowners.rb
+++ db/migrate/20190123015147_create_homeowners.rb
@@ -1,7 +1,8 @@
 class CreateHomeowners < ActiveRecord::Migration
   def change
   	create_table :homeowners do |t|
-  		t.string :name
+  		t.string :first_name
+  		t.string :last_name
   		t.string :email
   		t.string :password_digest
   		t.timestamps null: false
