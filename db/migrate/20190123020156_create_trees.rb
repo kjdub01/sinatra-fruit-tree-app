70590ff0e8441a0d216578bf790c4a8a70108011
kate jewett-williams <kjewettwilliams@gmail.com>
2019-01-22 19:09:30 -0700

    update tree table migration

--- db/migrate/20190123020156_create_trees.rb
+++ db/migrate/20190123020156_create_trees.rb
@@ -1,8 +1,10 @@
 class CreateTrees < ActiveRecord::Migration
   def change
     create_table :trees do |t|
-
-      t.timestamps null: false
+    	t.string :variety
+     	t.string :size
+     	t.string :fruit_weight
+     	t.timestamps null: false
     end
   end
 end
