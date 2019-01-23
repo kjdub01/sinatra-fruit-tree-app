87aeefaa7011c99cb8f19e453f8bdd52d771467e
kate jewett-williams <kjewettwilliams@gmail.com>
2019-01-22 22:05:42 -0700

    create get and post login

--- app/controllers/homeowners_controller.rb
+++ app/controllers/homeowners_controller.rb
@@ -8,5 +8,19 @@ class HomeownerController < ApplicationController
   	erb :login
   end
 
+  post '/login' do #(setting sessions)
+  	@homeowner = Homeowner.find_by(email: params[:email])
 
+  	if @homeowner && @homeowner.authenticate(params[:password])
+  		session[:homeowner_id] = @homeowner.id
+  	
+  		redirect "homeowners/@{homeowner.id}"
+  	else
+
+  	end
+  end
+
+  get '/homeowners/:id' do 
+  	"homeowners show page"
+  end
 end
