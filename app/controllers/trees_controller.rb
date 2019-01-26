ea30190c5b8713c81c5f837a33466dc91ea41a7f
kate jewett-williams <kjewettwilliams@gmail.com>
2019-01-26 15:04:34 -0700

    add post route

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -5,6 +5,15 @@ class TreesController < ApplicationController
 	end
 
 	post '/trees' do
-		
+	  if !logged_in?
+	  	redirect '/'
+	  end
+
+	  if params[:vareity] != "" && params[:size] != "" && params[:fruit_weight] != ""
+	  	@tree = Tree.create(variety: params[:variety], size: params[:size], fruit_weight: params[:fruit_weight], homeowner_id: current_user.id)
+	  	redirect "/trees/#{@tree.id}"
+	  else
+	  	redirect '/trees/new'
+	  end
 	end
 end
