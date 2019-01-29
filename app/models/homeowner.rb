0f5eafad42856a044e3abbeed0724965ea3a9fb8
kate jewett-williams <kjewettwilliams@gmail.com>
2019-01-28 21:29:06 -0700

    add AR validations

--- app/models/homeowner.rb
+++ app/models/homeowner.rb
@@ -1,4 +1,8 @@
 class Homeowner < ActiveRecord::Base
 	has_secure_password
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, uniqueness: true
+	
 	has_many :trees
 end
