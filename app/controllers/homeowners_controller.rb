63a244b07886496adbfc5f24ff61ccf915f43f03
kate jewett-williams <kjewettwilliams@gmail.com>
2019-01-29 15:58:57 -0700

    add redirect_if_not_logged in method

--- app/controllers/homeowners_controller.rb
+++ app/controllers/homeowners_controller.rb
@@ -33,13 +33,15 @@ class HomeownersController < ApplicationController
   	end
   end
 
-  get '/homeowners' do 
+  get '/homeowners' do
+    redirect_if_not_logged_in 
     @homeowners = Homeowner.all
 
     erb :'homeowners/index'
   end
 
   get '/homeowners/:id' do #(show homeowners show.erb)
+    redirect_if_not_logged_in
     @homeowner = Homeowner.find_by(id: params[:id])
 
   	erb :'homeowners/show'
