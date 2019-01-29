37dfb2b48f4943d83657a0c51f5f85c12a79f86a
kate jewett-williams <kjewettwilliams@gmail.com>
2019-01-29 16:27:44 -0700

    move signup, login, and logout routes to sessionscontroler

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,40 @@
+class SessionsController < ApplicationController
+
+  get '/signup' do #(show signup.erb) 
+    erb :'sessions/signup'
+  end
+
+  post '/homeowners' do 
+    @homeowner = Homeowner.new(params)
+    if @homeowner.save
+      session[:homeowner_id] = @homeowner.id
+
+      redirect "/homeowners/#{@homeowner.id}"
+    else
+      flash[:errors] = "Account creation failure: #{@Homeowner.errors.full_messages.to_sentence}"
+      redirect '/signup' #(need to include flash message)
+    end
+  end
+
+  get '/login' do #(show login.erb)
+  	erb :'sessions/login'
+  end
+
+  post '/login' do #(setting sessions)
+  	@homeowner = Homeowner.find_by(email: params[:email])
+
+  	if @homeowner && @homeowner.authenticate(params[:password])
+  		session[:homeowner_id] = @homeowner.id
+  	
+  		redirect "homeowners/#{@homeowner.id}"
+  	else
+      flash[:errors] = "Your email or password is inccorect. Please try again or Sign Up to continue"
+      redirect "/login"
+  	end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end  
+end
