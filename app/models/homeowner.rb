1488122b62a3475ea1b85a7e5ac21cecb70355b9
kate jewett-williams <kjewettwilliams@gmail.com>
2019-02-01 11:54:14 -0700

    add validation for email

--- app/models/homeowner.rb
+++ app/models/homeowner.rb
@@ -2,6 +2,7 @@ class Homeowner < ActiveRecord::Base
 	has_secure_password
 	validates :first_name, presence: true
 	validates :last_name, presence: true
+	validates :email, presence: true
 	validates :email, uniqueness: true
 	
 	has_many :trees
